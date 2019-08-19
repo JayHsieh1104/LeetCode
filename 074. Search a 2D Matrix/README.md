05895e0323b61ea8d94b63c1c5d3b922693dca4c
Jay Hsieh <jj011644@gmail.com>
2019-08-18 20:20:51 -0700

    Edit README.md

--- 074. Search a 2D Matrix/README.md
+++ 074. Search a 2D Matrix/README.md
@@ -1,6 +1,31 @@
-Solution 1：
+# 074. Search a 2D Matrix
+## Solution 1
+View the 2D matrix as a array and then just use binary search to search it.
+* Time Complexity: O(logM+logN)
+* Space Complexity: O(1)
 
+### Pros:
+1. Straightforward
+2. Not need to care about boundary problem
 
+### Cons:
+1. `hi = rowNum * colNum - 1` may cause overflow for large rowNum and colNum
+2. / and % are expensive operation
+
+## Solution 2
+Use binary search to search row first, and then search columns in the row.
+* Time Complexity: O(logM+logN)
+* Space Complexity: O(1)
+
+### Pros:
+1. Can be applied to large row and column, not lead to overflow
+2. 
+
+### Cons:
+1. **Need to care about boundary problem**
+2. 
+
+### ToDo
 Solution 2：注意二元搜尋到底後的停止位置？ 待解決
 https://leetcode.com/problems/search-a-2d-matrix/discuss/26220/Don't-treat-it-as-a-2D-matrix-just-treat-it-as-a-sorted-list
 			Ex. [[1,3,5,7],[10,11,16,20],[23,30,34,50]]
--- 074. Search a 2D Matrix/solution-1.java
+++ 074. Search a 2D Matrix/solution-1.java
@@ -1,26 +1,33 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         
+        // Take care of boundary testcase
         if(matrix.length == 0 || matrix[0].length == 0 || matrix == null) {
             return false;
         }
         
+        // Get row, column value
         int rowNum = matrix.length;
         int colNum = matrix[0].length;
         
+        // Calculate lo, hi value for binary search
         int lo = 0, hi = rowNum * colNum - 1;
         while(lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
+            int mid = lo + (hi - lo) / 2; // Avoid mid value overflow
+            // Find target
             if(matrix[mid / colNum][mid % colNum] == target) {
                 return true;
             }
+            // Search left side
             else if(matrix[mid / colNum][mid % colNum] > target) {
                 hi = mid - 1;
             }
+            // Search right side
             else {
                 lo = mid + 1;
             }
         }
+        // Not find the target
         return false;
     }
 }
