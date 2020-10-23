16f5fd9484775b6778071bd4430286a364559d50
JayHsieh1104 <jay.hsieh.cs@gmail.com>
2020-10-23 03:07:25 -0700

    Update 015

--- 015. 3Sum/README.md
+++ 015. 3Sum/README.md
@@ -4,6 +4,22 @@
 
 First, sort the nums array. Next, fix the first variable and use two pointers, one from the head of the rest of the array, another from the back of the rest of the array to estimate the sum of the three variables are zero or not.
 
-* Time Complexity: O(N^2)
+- Time Complexity: O(N^2)
 
-* Space Complexity: O(N)
+- Space Complexity: O(N), used for sorting
+
+## Solution 2 -- Heshset
+
+Fix one number and simplify the problem as a two sum problem.
+
+- Time Complexity: O(N^2)
+
+- Space Complexity: O(N), used for sorting
+
+## Solution 3 -- Heshset (No-sort)
+
+TODO:
+
+- Time Complexity: O(N^2)
+
+- Space Complexity: O(N)
--- 015. 3Sum/solution-1.py
+++ 015. 3Sum/solution-1.py
@@ -1,30 +1,32 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        output = []
+        if len(nums) < 3:
+            return output
+
         nums.sort()
-        
-        for i in range(len(nums) - 2):
+        for i in range(len(nums)-2):
             if nums[i] > 0:
                 break
-                
-            if i > 0 and nums[i] == nums[i - 1]:
+
+            if nums[i] == nums[i-1] and i > 0:
                 continue
-                
-            target = 0 - nums[i]
-            pointer1 = i + 1
-            pointer2 = len(nums) - 1
-            while pointer1 < pointer2:
-                if nums[pointer1] + nums[pointer2] == target:
-                    res.append([nums[i], nums[pointer1], nums[pointer2]])     
-                    while pointer1 < pointer2 and nums[pointer1] == nums[pointer1 + 1]:
-                        pointer1 += 1
-                    while pointer1 < pointer2 and nums[pointer2] == nums[pointer2 - 1]:
-                        pointer2 -= 1
-                    pointer1 += 1
-                    pointer2 -= 1
-                elif nums[pointer1] + nums[pointer2] < target:
-                    pointer1 += 1
+
+            target = -nums[i]
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                if nums[j] + nums[k] == target:
+                    output.append((nums[i], nums[j], nums[k]))
+                    j += 1
+                    k -= 1
+                    while j < k and nums[j-1] == nums[j]:
+                        j += 1
+                    while j < k and nums[k] == nums[k+1]:
+                        k -= 1
+                elif nums[j] + nums[k] < target:
+                    j += 1
                 else:
-                    pointer2 -= 1
-                   
-        return res
+                    k -= 1
+
+        return output
