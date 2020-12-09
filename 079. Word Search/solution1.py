138c54f56f03460f265e8f1816a2d1e3d1aadfe6
JayHsieh1104 <jay.hsieh.cs@gmail.com>
2020-12-09 02:24:05 -0800

    Update problem 079

--- 079. Word Search/solution1.py
+++ 079. Word Search/solution1.py
@@ -1,25 +1,20 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
 
-        def backtracking(row, col, suffix):
-            if suffix == '':
+        def backtracking(row, col, remained_word):
+            if remained_word == "":
                 return True
-            
-            if row < 0 or row >= len(board) or col < 0 or col >= len(board[0]) or board[row][col] != suffix[0]:
+            elif row < 0 or row >= len(board) or col < 0 or col >= len(board[0]) or board[row][col] != remained_word[0]:
                 return False
-            
-            board[row][col] = '#'
-            
-            for rowOffset, colOffset in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-                ret = backtracking(row+rowOffset, col+colOffset, suffix[1:])
-                if ret:
-                    break
-            
-            board[row][col] = suffix[0]
-            return ret    
-            
+            else:
+                board[row][col] = "#"
+                for row_offset, col_offset in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    if backtracking(row + row_offset, col + col_offset, remained_word[1:]):
+                        return True
+                board[row][col] = remained_word[0]
+
         for row in range(len(board)):
             for col in range(len(board[0])):
-                    if backtracking(row, col, word):
-                        return True
+                if backtracking(row, col, word):
+                    return True
         return False
