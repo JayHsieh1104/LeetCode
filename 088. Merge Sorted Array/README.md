dfa075a836ebd4137b3dc243c742027657b83ed9
JayHsieh1104 <jay.hsieh.cs@gmail.com>
2020-12-11 00:49:02 -0800

    Updated 088

--- 088. Merge Sorted Array/README.md
+++ 088. Merge Sorted Array/README.md
@@ -1,11 +1,9 @@
 # 088. Merge Sorted Array
-## Solution 1 -- Optimal solution
+
+## Solution 1 -- Two pointer (start from end)
+
 Use two pointer to select the bigger element from the two array, and then place it from the right side of nums1
+
 * Time Complexity: O(N)
+
 * Space Complexity: O(1)
-
-## Solution 2
-Use two pointer to select the bigger element from the two array, and then place it from the left side of nums1
-* Time Complexity: O(N)
-* Space Complexity: O(N)
-
--- 088. Merge Sorted Array/solution-1.py
+++ 088. Merge Sorted Array/solution-1.py
@@ -3,12 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        for cur in range(len(nums1) - 1, -1, -1):
-            if n == 0:
-                break
-            elif m == 0 or nums2[n - 1] >= nums1[m - 1]:
-                nums1[cur] = nums2[n - 1]
+        current_pos = len(nums1) - 1
+        while n > 0:
+            if m == 0 or nums2[n-1] >= nums1[m-1]:
+                nums1[current_pos] = nums2[n-1]
                 n -= 1
             else:
-                nums1[cur] = nums1[m - 1]
+                nums1[current_pos] = nums1[m-1]
                 m -= 1
+            current_pos -= 1
+        
