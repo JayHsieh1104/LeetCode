06d06c2207011471940b00aa56b3565eec6ba742
JayHsieh1104 <jay.hsieh.cs@gmail.com>
2020-12-12 20:31:29 -0800

    Add problem 865

---
+++ 865. Smallest Subtree with all the Deepest Nodes/README.md
@@ -0,0 +1,15 @@
+# 865. Smallest Subtree with all the Deepest Nodes
+
+## Solution 1 -- Record the depth of each nodes and find the lowest common ancestor
+
+* Time Complexity: O(N), where N is the number of nodes in the tree.
+
+* Space Complexity: O(N)
+
+## Solution 2 -- Recursion
+
+Combine the dfs and answer functions in solution 1 into one function
+
+* Time Complexity: O(N), where N is the number of nodes in the tree.
+
+* Space Complexity: O(N)
---
+++ 865. Smallest Subtree with all the Deepest Nodes/solution2.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def subtreeWithAllDeepest(self, root: TreeNode) -> TreeNode:
+        
+        def dfs(node):
+            if node == None:
+                return (None, 0)
+            else:
+                left_child, right_child = dfs(node.left), dfs(node.right)
+                if left_child[1] == right_child[1]:
+                    return (node, left_child[1] + 1)
+                elif left_child[1] > right_child[1]:
+                    return (left_child[0], left_child[1] + 1)
+                elif right_child[1] > left_child[1]:
+                    return (right_child[0], right_child[1] + 1)
+
+        return dfs(root)[0]
