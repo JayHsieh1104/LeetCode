9ec93384a5aa304631378f6a04fcdff2a05f0d72
JayHsieh1104 <jay.hsieh.cs@gmail.com>
2020-12-14 02:37:22 -0800

    Update problem 033

--- 033. Search in Rotated Sorted Array/README.md
+++ 033. Search in Rotated Sorted Array/README.md
@@ -1,5 +1,7 @@
 # 033. Search in Rotated Sorted Array
-## Solution 1
-Unfinished
+
+## Solution 1 - Binary Search
+
 * Time Complexity: O(logN)
+
 * Space Complexity: O(1)
--- 033. Search in Rotated Sorted Array/solution1.py
+++ 033. Search in Rotated Sorted Array/solution1.py
@@ -1,19 +1,18 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        begin = 0
-        end = len(nums) - 1
-        while begin <= end:
-            mid = (begin + end) // 2
+        start, end = 0, len(nums) - 1
+        while start <= end:
+            mid = start + (end - start) // 2
             if nums[mid] == target:
                 return mid
-            if nums[mid] > nums[end]: # left side of mid is sorted
-                if nums[begin] <= target and target < nums[mid]:
+            elif nums[mid] >= nums[start]:
+                if nums[start] <= target and target < nums[mid]:
                     end = mid - 1
                 else:
-                    begin = mid + 1
+                    start = mid + 1
             else:
                 if nums[mid] < target and target <= nums[end]:
-                    begin = mid + 1
+                    start = mid + 1
                 else:
                     end = mid - 1
         return -1
