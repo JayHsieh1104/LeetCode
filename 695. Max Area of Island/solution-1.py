d405e8b22d05f05ed998122e5010ab88b1ddf077
JayHsieh1104 <jay.hsieh.cs@gmail.com>
2020-12-20 02:00:23 -0800

    Update problem 695

--- 695. Max Area of Island/solution-1.py
+++ 695. Max Area of Island/solution-1.py
@@ -1,22 +1,24 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        visited = set()
-        maximum_area = 0
-        
-        for r0, row in enumerate(grid):
-            for c0, val in enumerate(row):
-                if val and (r0, c0) not in visited:
-                    shape = 0
-                    stack = [(r0, c0)]
-                    visited.add((r0, c0))
+        max_area = 0
+
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                curr_area = 0
+                stack = []
+                if grid[row][col] == 1:
+                    stack.append((row, col))
                     while stack:
-                        r, c = stack.pop()
-                        shape += 1
-                        for nr, nc in ((r-1, c), (r+1, c), (r, c-1), (r, c+1)):
-                            if (0 <= nr < len(grid) and 0 <= nc < len(grid[0])
-                                and grid[nr][nc] and (nr, nc) not in visited):
-                                stack.append((nr, nc))
-                                visited.add((nr, nc))
-                    maximum_area = max(maximum_area, shape)
-                    
-        return maximum_area
+                        curr_row, curr_col = stack.pop()
+                        curr_area += 1
+                        grid[curr_row][curr_col] = 0
+
+                        for row_offset, col_offset in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                            if curr_row + row_offset >= 0 and curr_row + row_offset < len(grid) and curr_col + col_offset >= 0 and curr_col + col_offset < len(grid[0]) and grid[curr_row + row_offset][curr_col + col_offset] == 1:
+                                stack.append(
+                                    (curr_row + row_offset, curr_col + col_offset))
+                                grid[curr_row + row_offset][curr_col +
+                                                            col_offset] = 0
+                max_area = max(max_area, curr_area)
+
+        return max_area
