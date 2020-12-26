d5828e6fa9847a13faf4f6efc068e365f17a13fb
JayHsieh1104 <jay.hsieh.cs@gmail.com>
2020-12-26 03:31:20 -0800

    Update problem 084

--- 084. Largest Rectangle in Histogram/solution3.py
+++ 084. Largest Rectangle in Histogram/solution3.py
@@ -1,14 +1,14 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        
-        def calculateArea(start, end):
-            if start > end:
+
+        def calculateArea(lo, hi):
+            if lo > hi:
                 return 0
-            
-            min_height_pos = start
-            for i in range(start, end+1):
-                if heights[i] < heights[min_height_pos]:
-                    min_height_pos = i
-            return max(heights[min_height_pos] * (end - start + 1), calculateArea(start, min_height_pos-1), calculateArea(min_height_pos+1, end))
-        
-        return calculateArea(0, len(heights)-1)
+            else:
+                min_height_pos = lo
+                for i in range(lo + 1, hi + 1):
+                    if heights[i] < heights[min_height_pos]:
+                        min_height_pos = i
+                return max(heights[min_height_pos] * (hi - lo + 1), calculateArea(lo, min_height_pos - 1), calculateArea(min_height_pos + 1, hi))
+
+        return calculateArea(0, len(heights) - 1)
--- 084. Largest Rectangle in Histogram/solution4.py
+++ 084. Largest Rectangle in Histogram/solution4.py
@@ -1,14 +1,12 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-  
-        ret = 0
-        ascending_stack = [-1]
         heights.append(0)
+        ascending_stack = [-1]
+        max_area = 0
         
         for i in range(len(heights)):
-            while heights[ascending_stack[-1]] > heights[i]:
-                ret = max(ret, heights[ascending_stack.pop()] * (i - 1 - ascending_stack[-1]))
+            while heights[i] < heights[ascending_stack[-1]]:
+                max_area = max(max_area, heights[ascending_stack.pop()] * (i - 1 - ascending_stack[-1]))
             ascending_stack.append(i)
-        
-        heights.pop()
-        return ret
+            
+        return max_area
