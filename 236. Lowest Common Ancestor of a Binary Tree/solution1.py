eb0d8a43ca4450ddeea5f7d2eb87f80a1fbd04f7
JayHsieh1104 <jay.hsieh.cs@gmail.com>
2020-12-28 23:45:24 -0800

    Update solution1.py

--- 236. Lowest Common Ancestor of a Binary Tree/solution1.py
+++ 236. Lowest Common Ancestor of a Binary Tree/solution1.py
@@ -7,20 +7,21 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        self.ans = None
-
-        def has_target(curr_node):
-            if curr_node == None:
+        def __init__(self):
+            self.ans = None
+        
+        def recurse_tree(current_node: 'TreeNode') -> 'boolean':
+            if current_node == None:
                 return False
-
-            mid = curr_node == p or curr_node == q
-            left = has_target(curr_node.left)
-            right = has_target(curr_node.right)
-
+            
+            mid = current_node.val == p.val or current_node.val == q.val
+            left = recurse_tree(current_node.left)
+            right = recurse_tree(current_node.right)
+            
             if mid + left + right == 2:
-                self.ans = curr_node
-
+                self.ans = current_node
+            
             return mid or left or right
-
-        has_target(root)
-        return self.ans
+        
+        recurse_tree(root)
+        return self.ans            
