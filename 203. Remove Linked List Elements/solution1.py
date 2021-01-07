e941d589a5d66b06dc0531fc50ebb233e7ffa5b5
JayHsieh1104 <jay.hsieh.cs@gmail.com>
2021-01-07 10:24:31 -0800

    Update solution1.py

--- 203. Remove Linked List Elements/solution1.py
+++ 203. Remove Linked List Elements/solution1.py
@@ -5,16 +5,15 @@
 #         self.next = next
 class Solution:
     def removeElements(self, head: ListNode, val: int) -> ListNode:
-        dummyHead = ListNode()
+        dummyHead = ListNode(0)
         dummyHead.next = head
         
         prev, curr = dummyHead, head
-        
-        while curr != None:
+        while curr:
             if curr.val == val:
                 prev.next = curr.next
             else:
                 prev = curr
             curr = curr.next
-                
+        
         return dummyHead.next
