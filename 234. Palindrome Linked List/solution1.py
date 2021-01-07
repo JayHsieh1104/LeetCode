17f8bf3d9a1c174f63af8bca2d7f1da4dd97d612
JayHsieh1104 <jay.hsieh.cs@gmail.com>
2021-01-07 01:34:33 -0800

    Update solution1.py

--- 234. Palindrome Linked List/solution1.py
+++ 234. Palindrome Linked List/solution1.py
@@ -5,15 +5,15 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: ListNode) -> bool:
-        self.front_pointer = head
+        def is_next_same(node: ListNode) -> bool:
+            nonlocal head
+            if node == None:
+                return True
+            
+            if is_next_same(node.next) and head.val == node.val:
+                head = head.next
+                return True
+            else:
+                return False
         
-        def recursively_check(current_node=head):
-            if current_node is not None:
-                if not recursively_check(current_node.next):
-                    return False
-                if current_node.val != self.front_pointer.val:
-                    return False
-                self.front_pointer = self.front_pointer.next
-            return True
-        
-        return recursively_check()
+        return is_next_same(head)
