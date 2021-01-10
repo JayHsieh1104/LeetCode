0f97ee0955aacea25f90ed020d89a07a1966e46b
JayHsieh1104 <jay.hsieh.cs@gmail.com>
2021-01-09 22:29:03 -0800

    Update problem 148

--- 148. Sort List/README.md
+++ 148. Sort List/README.md
@@ -2,13 +2,13 @@
 
 ## Solution 1 -- Merge Sort (Top to Bottom)
 
-* Time Complexity: O(NlogN), N is the length of the linked list.
+* Time Complexity: O(NlogN), where N is the length of the linked list.
 
 * Space Complexity: O(logN), used for do recursion in stack memory.
 
-## Solution 2 -- 
+## Solution 2 -- Merge Sort (Bottom to Top)
 
-TODO:
+TODO: Hard Problem.
 
 * Time Complexity: O(NlogN), N is the length of the linked list.
 
--- 148. Sort List/solution1.py
+++ 148. Sort List/solution1.py
@@ -5,46 +5,42 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: ListNode) -> ListNode:
-        
-        def merge(head1, head2):
-            dummyHead = ListNode()
-            pointer = dummyHead
-            
-            while head1 != None and head2 != None:
-                if head1.val > head2.val:
-                    pointer.next = ListNode(head2.val)
-                    head2 = head2.next
-                else:
-                    pointer.next = ListNode(head1.val)
-                    head1 = head1.next
-                pointer = pointer.next
-            
-            while head1 != None:
-                pointer.next = ListNode(head1.val)
-                pointer = pointer.next
-                head1 = head1.next
-
-            while head2 != None:
-                pointer.next = ListNode(head2.val)
-                pointer = pointer.next
-                head2 = head2.next
-
-            return dummyHead.next
-
         if head == None or head.next == None:
             return head
-
-        # 1. Cut the list to two sublists
-        prev, slow, fast = None, head, head
         
-        while fast != None and fast.next != None:
-            prev, slow, fast = slow, slow.next, fast.next.next
+        mid = self.getMid(head)
+        left = self.sortList(head)
+        right = self.sortList(mid)
+        return self.merge(left, right)
+    
+    def getMid(self, head: ListNode) -> ListNode:
+        midPrev = None
+        while head and head.next:
+            if midPrev == None:
+                midPrev = head
+            else:
+                midPrev = midPrev.next
+            head = head.next.next
+        mid = midPrev.next
+        midPrev.next = None
+        return mid
         
-        prev.next = None
-
-        # 2. Sort the two sublists
-        L1 = self.sortList(head)
-        L2 = self.sortList(slow)
-
-        # 3. Merge the two sublists
-        return merge(L1, L2)
+        
+    def merge(self, list1: ListNode, list2: ListNode) -> ListNode:
+        dummyHead = ListNode(0)
+        curr = dummyHead
+        while list1 and list2:
+            if list1.val < list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+                
+        if list1:
+            curr.next = list1
+        else:
+            curr.next = list2
+                
+        return dummyHead.next
