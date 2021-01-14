fdb6d21f41ccf717c143d96dcbd25983eb3cb14c
JayHsieh1104 <jay.hsieh.cs@gmail.com>
2021-01-13 22:44:55 -0800

    Update problem 953

--- 953. Verifying an Alien Dictionary/solution1.py
+++ 953. Verifying an Alien Dictionary/solution1.py
@@ -1,17 +1,21 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        char_weight = {c: i for i, c in enumerate(order)}
+        order_dict = {}
+        for i in range(len(order)):
+            order_dict[order[i]] = i
         
-        for i in range(len(words)-1):
-            if len(words[i]) > len(words[i+1]) and words[i][:len(words[i+1])] == words[i+1]:
-                return False
-            else:   
-                for j in range(len(words[i])):
-                    if char_weight[words[i][j]] < char_weight[words[i+1][j]]:
-                        break
-                    elif char_weight[words[i][j]] == char_weight[words[i+1][j]]:
-                        continue
-                    elif char_weight[words[i][j]] > char_weight[words[i+1][j]]:
+        for i in range(1, len(words)):
+            word1 = words[i-1]
+            word2 = words[i]
+            
+            for j in range(min((len(word1), len(word2)))):
+                if word1[j] != word2[j]:
+                    if order_dict[word1[j]] > order_dict[word2[j]]:
                         return False
-                
+                    else:
+                        break
+            
+            if word1[:min((len(word1), len(word2)))] == word2[:min((len(word1), len(word2)))] and len(word1) > len(word2):
+                return False
+            
         return True
