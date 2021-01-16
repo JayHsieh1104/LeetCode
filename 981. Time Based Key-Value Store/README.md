afeb87674f6a3396ae02145277d10a43405e8c65
JayHsieh1104 <jay.hsieh.cs@gmail.com>
2021-01-16 02:12:27 -0800

    Update problem 981

--- 981. Time Based Key-Value Store/README.md
+++ 981. Time Based Key-Value Store/README.md
@@ -7,11 +7,3 @@
   * Set: O(1)
   
 * Space Complexity: O(N)
-
-## Solution 2 -- TreeMap
-
-The idea of the solution is like No. 200, Number of Islands. Use BFS to rot the adjacent cells. If there is a fresh orange which can't be rotten. Then return -1
-
-* Time Complexity: O(N), N is the number of the toal grid cells
-
-* Space Complexity: O(N)
--- 981. Time Based Key-Value Store/solution1.py
+++ 981. Time Based Key-Value Store/solution1.py
@@ -1,33 +1,30 @@
-from collections import defaultdict
-import bisect
-
 class TimeMap:
 
     def __init__(self):
-        self.mDict = defaultdict(list)
-        
+        """
+        Initialize your data structure here.
+        """
+        self.mDict = collections.defaultdict(list)
+
     def set(self, key: str, value: str, timestamp: int) -> None:
-            self.mDict[key].append((timestamp, value))
+        self.mDict[key].append((value, timestamp))
+            
 
     def get(self, key: str, timestamp: int) -> str:
         if key not in self.mDict:
             return ""
         else:
-            lo = 0
-            hi = len(self.mDict[key])
-            
+            lo, hi = 0, len(self.mDict[key])
             while lo < hi:
                 mid = lo + (hi - lo) // 2
-                if self.mDict[key][mid][0] <= timestamp:
+                if self.mDict[key][mid][1] <= timestamp:
                     lo = mid + 1
                 else:
                     hi = mid
-            
             if hi == 0:
                 return ""
             else:
-                return self.mDict[key][hi - 1][1]
-
+                return self.mDict[key][hi - 1][0]
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
