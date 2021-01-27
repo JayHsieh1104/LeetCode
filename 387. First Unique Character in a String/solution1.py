af7db0927ca73e55ed4e2418fc91e532389a12f8
JayHsieh1104 <jay.hsieh.cs@gmail.com>
2021-01-26 23:27:08 -0800

    Update solution1.py

--- 387. First Unique Character in a String/solution1.py
+++ 387. First Unique Character in a String/solution1.py
@@ -1,17 +1,11 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        seen = set()
-        letter_frequency = {}
-
+        seen = collections.defaultdict(int)
         for i in range(len(s)):
-            if s[i] not in seen:
-                seen.add(s[i])
-                letter_frequency[s[i]] = i
-            elif s[i] in letter_frequency:
-                del letter_frequency[s[i]]
+            seen[s[i]] += 1
         
-        if letter_frequency:
-            for key in letter_frequency.keys():
-                return letter_frequency[key]
-        else:
-            return -1
+        for i in range(len(s)):
+            if seen[s[i]] == 1:
+                return i
+        
+        return -1
